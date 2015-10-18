34ee0121080e8b1ab67085b11573a87ea49df83e
Francecil <396324491@qq.com>
2015-10-19 04:04:33 +0800

    读取pdf功能实现，注意gradle的配置

---
+++ app/src/main/java/com/france/sharereader/MainActivity.java
@@ -0,0 +1,26 @@
+package com.france.sharereader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.joanzapata.pdfview.PDFView;
+
+public class MainActivity extends Activity  {
+
+    public static final String SAMPLE_FILE = "sample.pdf";
+
+    public static final String ABOUT_FILE = "about.pdf";
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_main);
+        PDFView pdfView= (PDFView) findViewById(R.id.pdfView);
+        pdfView.fromAsset("sample.pdf")
+                .defaultPage(1)
+                .showMinimap(false)
+                .enableSwipe(true)
+                .load();
+    }
+
+
+}
