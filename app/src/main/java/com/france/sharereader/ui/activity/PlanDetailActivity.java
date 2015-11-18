560ad0e6e8328cd7bc9cc46cf7bff84d3861d1f9
Lareina_LUO <18450051237@163.com>
2015-11-19 00:18:01 +0800

    更新 图片以及timepicker

--- app/src/main/java/com/france/sharereader/ui/activity/PlanDetailActivity.java
+++ app/src/main/java/com/france/sharereader/ui/activity/PlanDetailActivity.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.france.sharereader.R;
+import com.france.sharereader.util.DateTimePickDialogUtil;
 
 import net.tsz.afinal.FinalActivity;
 import net.tsz.afinal.annotation.view.ViewInject;
@@ -27,20 +28,29 @@ public class PlanDetailActivity extends BaseActivity {
     private ImageView clock;
     @ViewInject(id = R.id.my_plan)
     private EditText plan_content;
-    private Boolean isEmpty = true;
+    @ViewInject(id=R.id.back_home)
+    private ImageView back_home;
+    @ViewInject(id=R.id.display_time)
+    private EditText display_time;
     private String plan_message;
     private String plan_title;
+    private String initDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan_detail);
         FinalActivity.initInjectedView(this);//实现IOC注解组件
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
+        initDateTime=sdf.format(new java.util.Date());
+        display_time.setText(initDateTime);
         Bundle bundle = this.getIntent().getExtras();
         plan_title = bundle.getString("title");
         title.setText(plan_title);
         initTextTitle();
         initEditText();
+        setTime();
+        back();
     }
 
     private void initTextTitle(){
@@ -63,6 +73,24 @@ public class PlanDetailActivity extends BaseActivity {
                 //获取当前文本
                 plan_message =clickEditText.getText().toString().trim();
                 return false;
+            }
+        });
+    }
+    private void setTime(){
+        clock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DateTimePickDialogUtil dateTimePicKDialog = new DateTimePickDialogUtil(
+                        PlanDetailActivity.this, initDateTime);
+                dateTimePicKDialog.dateTimePicKDialog(display_time);
+            }
+        });
+    }
+    private void back(){
+        back_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
