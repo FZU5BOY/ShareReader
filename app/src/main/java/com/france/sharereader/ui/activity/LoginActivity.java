912361a12ff7dbf9b5bb23c396af60c9ea33ca27
Lareina_LUO <18450051237@163.com>
2015-11-23 12:49:20 +0800

    add loginactivity

--- app/src/main/java/com/france/sharereader/ui/activity/LoginActivity.java
+++ app/src/main/java/com/france/sharereader/ui/activity/LoginActivity.java
@@ -3,5 +3,6 @@ package com.france.sharereader.ui.activity;
 /**
  * Created by Lareina on 2015/11/22.
  */
-public class LoginActivity {
+public class LoginActivity extends BaseActivity{
+         //test github;
 }
