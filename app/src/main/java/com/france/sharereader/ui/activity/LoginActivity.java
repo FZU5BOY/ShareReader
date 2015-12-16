7fe244e7a78a2e578b85e858387b1c7e66a45056
Lareina-LUO <574017121@qq.com>
2015-12-16 20:17:17 +0800

    commit 增加跳转到登录页面

--- app/src/main/java/com/france/sharereader/ui/activity/LoginActivity.java
+++ app/src/main/java/com/france/sharereader/ui/activity/LoginActivity.java
@@ -1,8 +1,21 @@
 package com.france.sharereader.ui.activity;
 
+import android.os.Bundle;
+
+import com.france.sharereader.R;
+
+import net.tsz.afinal.FinalActivity;
+import net.tsz.afinal.annotation.view.ViewInject;
+
 /**
  * Created by Lareina on 2015/11/22.
  */
 public class LoginActivity extends BaseActivity{
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        FinalActivity.initInjectedView(this);//实现IOC注解组件
+    }
 }
