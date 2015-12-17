1282a1b7330e436ac66329473f183da16c146c75
Lareina-LUO <574017121@qq.com>
2015-12-18 02:04:12 +0800

    comment achieve

--- app/src/main/java/com/france/sharereader/ui/activity/SettingActivity.java
+++ app/src/main/java/com/france/sharereader/ui/activity/SettingActivity.java
@@ -18,7 +18,6 @@ public class SettingActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_setting, menu);
         return true;
     }
 
