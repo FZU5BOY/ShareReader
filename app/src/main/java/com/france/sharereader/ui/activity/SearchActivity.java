b98560b6c2e81edd16257a47699a7e5e1f38faab
VIKO-HUANG <viko597963607@163.com>
2015-12-18 01:02:53 +0800

    搜索界面

---
+++ app/src/main/java/com/france/sharereader/ui/activity/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.france.sharereader.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.france.sharereader.R;
+
+
+
+public class SearchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+    }
+}
