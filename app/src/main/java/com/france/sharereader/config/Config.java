282bf7f83a6b033c9c91508acf46d08ac4df619e
Lareina-LUO <574017121@qq.com>
2016-05-10 10:57:44 +0800

    one

--- app/src/main/java/com/france/sharereader/config/Config.java
+++ app/src/main/java/com/france/sharereader/config/Config.java
@@ -6,7 +6,7 @@ import android.os.Environment;
  * Created by Administrator on 2015/11/20.
  */
 public  class Config {
-    public static String applicationId = "15378dde3425b680b6781bba68ff69b3";//这是Bmob的ApplicationId,用于初始化操作
+    public static String applicationId = "667ad374691382edeb12f4fdc226f061";//这是Bmob的ApplicationId,用于初始化操作
     static public String[] TOPICS = {"编译原理", "软件工程", "数据挖掘", "电子设计"};
     public static String extern = Environment.getExternalStorageDirectory().getPath();
 }
